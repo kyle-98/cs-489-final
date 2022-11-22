e2863ad93b9e074e4b614864420397e554f323f7
kyle <kforker1@gmail.com>
2022-11-22 14:34:59 -0500

    Update data.sql

--- data/data.sql
+++ data/data.sql
@@ -1,6 +1,9 @@
 CREATE TABLE hurricanes (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name MEDIUMTEXT NOT NULL,
-  year INT NULL,
-  coords MEDIUMTEXT NULL,
-  data MEDIUMTEXT NULL);
+  year INT NOT NULL,
+  coords MEDIUMTEXT NOT NULL,
+  data MEDIUMTEXT NOT NULL,
+  starting_month VARCHAR(30) NOT NULL,
+  highest_wind INT NOT NULL,
+  named_storm_days INT NOT NULL);
